023c2e842ff2512fc566ba8c7b82cf8d5410a765
Jeannuel Garcia <jjeanlink@gmail.com>
2018-09-23 00:56:31 -0500

    first commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBiO6PbmyQEsm_igx0MAeVjLGaQ0JnLB2Q",
+  authDomain: "farmacia-litofarma-94380.firebaseapp.com",
+  databaseURL: "https://farmacia-litofarma-94380.firebaseio.com",
+  projectId: "farmacia-litofarma-94380",
+  storageBucket: "farmacia-litofarma-94380.appspot.com",
+  messagingSenderId: "656933106230"
+};
